8d90c79f5ced7d70992e3ed0075f4d809ba93450
Douglas Grim <douglas.grim.123@gmail.com>
2024-06-15 15:37:55 -0700

    added object stringify to Room and Meeting

--- pythonProject/image_classifier/meetings/models.py
+++ pythonProject/image_classifier/meetings/models.py
@@ -7,11 +7,16 @@ class Room(models.Model):
     floor = models.IntegerField()
     room_number = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.name}: room {self.room_number} on floor {self.floor}"
+
 
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
     start_time = models.TimeField(default=time(9))
     duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return f"{self.title}"
